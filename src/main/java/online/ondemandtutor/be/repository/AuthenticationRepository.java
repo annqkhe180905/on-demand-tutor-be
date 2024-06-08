761ff890b2d4371cb0e2031360d7d506fbfa9a96
phongthuong12 <90915750+phongthuong12@users.noreply.github.com>
2024-06-08 11:18:06 +0700

    add login google

--- src/main/java/online/ondemandtutor/be/repository/AuthenticationRepository.java
+++ src/main/java/online/ondemandtutor/be/repository/AuthenticationRepository.java
@@ -3,12 +3,17 @@ package online.ondemandtutor.be.repository;
 
 import online.ondemandtutor.be.entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+
+@Repository
 public interface AuthenticationRepository extends JpaRepository<Account, Long> {
     List<Account> findAccountByIsDeletedFalse();
     Account findAccountByIdAndIsDeletedFalse(long id);
 
-    Account findAccountByPhone(String phone);
+
+
+    Account findAccountByEmail(String email);
 }
